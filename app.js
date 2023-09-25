f45760ea588e7473d759bd6e6b82ce42a71b01da
Raghav Singhal <raghav.singhal2001@gmail.com>
2023-09-25 20:11:39 +0530

    Vercel SetUp

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-raghav:Test123@cluster0.br5e4fh.mongodb.net/todolistDB",{useNewUrlParser:true});
 
 const itemsSchema={
   name:String
